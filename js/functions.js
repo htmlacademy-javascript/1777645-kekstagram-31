2c71a4cfec1ccc2d0fa7a992e3b9aae471676444
Dzmitry Herashchanka <gera.1212@yandex.ru>
2024-02-20 16:51:20 +0300

    feat: добавляет функцию проверки на полиэдром

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,13 @@
 const isLengthString = (string, length) => string.length <= length;
 
+const isPalindrome = (string) => {
+  let reverseString = '';
+
+  for (let i = 0; i < string.length; i++) {
+    reverseString += string.at(-i - 1)
+  }
+  return reverseString.toLowerCase().replaceAll(' ', '') === string.toLowerCase().replaceAll(' ', '');
+};
+
 isLengthString('проверяемая строка', 20);
+isPalindrome('Лёша на полке клопа нашёл ');

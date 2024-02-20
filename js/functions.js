ab5bf6dfc3f13aa525b3423ad2c25c8f08811533
Dzmitry Herashchanka <gera.1212@yandex.ru>
2024-02-20 16:52:00 +0300

    feat: добавляет функцию вывода положительного числа

--- js/functions.js
+++ js/functions.js
@@ -4,10 +4,27 @@ const isPalindrome = (string) => {
   let reverseString = '';
 
   for (let i = 0; i < string.length; i++) {
-    reverseString += string.at(-i - 1)
+    reverseString += string.at(-i - 1);
   }
   return reverseString.toLowerCase().replaceAll(' ', '') === string.toLowerCase().replaceAll(' ', '');
 };
 
+const getNumber = (data) => {
+  let result = '';
+
+  if (typeof (data) === 'number') {
+    return Math.abs(data.toString().replace('.', ''));
+  }
+
+  for (let i = 0; i < data.length; i++) {
+    if (!Number.isNaN(parseInt(data[i], 10))) {
+      result += data[i];
+    }
+  }
+
+  return parseInt(result, 10);
+};
+
 isLengthString('проверяемая строка', 20);
 isPalindrome('Лёша на полке клопа нашёл ');
+getNumber(-1.5);

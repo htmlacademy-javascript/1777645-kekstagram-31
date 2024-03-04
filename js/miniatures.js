0f5d7cb2b38ef2aa735ee58dd0be69b7084154aa
Dzmitry Herashchanka <gera.1212@yandex.ru>
2024-03-04 14:45:52 +0300

    feat: добавляет отображение полноразмерных изображений

--- js/miniatures.js
+++ js/miniatures.js
@@ -5,14 +5,17 @@ const blockPictures = document.querySelector('.pictures');
 const dataPhotos = createPhotos();
 const fragment = document.createDocumentFragment();
 
-dataPhotos.forEach(({url, description, likes, comments}) => {
+dataPhotos.forEach(({id, url, description, likes, comments}) => {
   const photoElement = pictureTemplate.cloneNode(true);
   photoElement.querySelector('.picture__img').src = url;
   photoElement.querySelector('.picture__img').alt = description;
   photoElement.querySelector('.picture__comments').textContent = comments.length;
   photoElement.querySelector('.picture__likes').textContent = likes;
+  photoElement.dataset.id = id;
 
   fragment.append(photoElement);
 });
 
 blockPictures.append(fragment);
+
+export {dataPhotos};
